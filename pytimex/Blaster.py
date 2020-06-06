33c82787f9ec28563b88af01ee05dc20526cb42e
Simon Larsson <snigelson@gmail.com>
2020-06-06 20:56:14 +0200

    Commit version which actually works with commited Arduino code
    Buffers everything until flush is called

--- pytimex/Blaster.py
+++ pytimex/Blaster.py
@@ -4,43 +4,16 @@
 import serial
 import sys
 import time
+import struct
 
 class Blaster:
 	def __init__(self, portname, syncbuflen=16):
 		self.port = serial.Serial(portname, 9600, timeout=0.1)
 		time.sleep(2)
-		self.sent = []
-		self.syncbuflen = syncbuflen
+		self.to_send = []
 
 	def identify(self):
-		self.port.write(b'x')
-		indata = self.port.read(1)
-		if indata != b'x':
-			raise Exception("Transceiver not detected! (x error)")
-			return False
-
-		self.port.write(b'?')
-		time.sleep(0.2)
-		indata = self.port.read(5)
-		if indata != b'M764\0':
-			print (indata)
-			raise Exception("Transceiver not detected! (? error)")
-			return False
-
 		return True
-
-	def _checksync(self):
-#		print("Sent: "+str(self.sent))
-
-		inbytes = self.port.read(int(self.syncbuflen/2))
-		if len(inbytes) == 0:
-			return
-#		print("Syncing {} byte(s)".format(len(inbytes)))
-		for byt in inbytes:
-			if byt != self.sent[0]:
-				print("Expected {}, got {}".format(self.sent[0], byt))
-				raise Exception("Out of sync!")
-			self.sent.pop(0)
 
 	def send_sync(self, times55sync=128, timesAAsync=50):
 		for sync in range(times55sync):
@@ -50,15 +23,13 @@ class Blaster:
 			self.blast(0xAA)
 
 	def blast(self, data):
-		self.port.write(bytes([data]))
-		self.sent.append(data)
-
-		if len(self.sent)>self.syncbuflen:
-			self._checksync()
+		self.to_send.append(data)
 
 	def flush(self):
-		while len(self.sent)>0:
-			self._checksync()
+		self.port.write(struct.pack('!H', len(self.to_send)))
+		self.port.write(self.to_send)
+
+
 
 if __name__ == "__main__":
 	portname = sys.argv[1]
